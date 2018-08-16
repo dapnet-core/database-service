662208cc1baf4cb56e0ebce1334a210885de80c6
Philipp Thiel <taronyu@users.noreply.github.com>
2018-08-16 19:53:02 +0200

    Added json transformation

--- src/main/java/de/hampager/dapnet/service/database/UserController.java
+++ src/main/java/de/hampager/dapnet/service/database/UserController.java
@@ -1,6 +1,9 @@
 package de.hampager.dapnet.service.database;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +22,28 @@ class UserController {
     private final RestTemplate users;
 
     @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
     public UserController(RestTemplateBuilder builder) {
         users = builder.basicAuthorization(DB_USER, DB_PASS).build();
     }
 
     @GetMapping
     public ResponseEntity<JsonNode> getAll() {
-        return users.getForEntity(BASE_URL, JsonNode.class);
-        //return ResponseEntity.ok(n);
+        JsonNode node = users.getForObject(BASE_URL, JsonNode.class);
+        ObjectNode out = mapper.createObjectNode();
+
+        out.put("total_rows", node.get("total_rows").asInt());
+        ArrayNode rows = out.putArray("rows");
+        for (JsonNode n : node.get("rows")) {
+            JsonNode doc = n.get("doc");
+            ((ObjectNode) doc).remove("password");
+            rows.add(doc);
+        }
+
+        return ResponseEntity.ok(out);
+        //return users.getForEntity(BASE_URL, JsonNode.class);
     }
 
 }
