da771466bae6688a801823996d68cb9bba9a9900
Ralf Wilke <wilke@ihf.rwth-aachen.de>
2018-10-25 15:16:03 +0200

    Added "messages" to VALID_KEYS_UPDATE on RubricController. ToDo: Check that the number of Strings in that list is exact 10.

--- src/main/java/de/hampager/dapnet/service/database/controller/RubricController.java
+++ src/main/java/de/hampager/dapnet/service/database/controller/RubricController.java
@@ -45,7 +45,7 @@ class RubricController extends AbstractController {
 
 	private static final Logger logger = LoggerFactory.getLogger(RubricController.class);
 	private static final Set<String> VALID_KEYS_UPDATE = Set.of("number", "description", "label", "transmitter_groups",
-			"transmitters", "cyclic_transmit", "cyclic_transmit_interval", "owners");
+			"transmitters", "cyclic_transmit", "cyclic_transmit_interval", "owners", "messages");
 	private static final String[] REQUIRED_KEYS_CREATE = { "_id", "number", "label", "description",
 			"transmitter_groups", "transmitters", "owners" };
 	private static final String RUBRIC_LIST = "rubric.list";
